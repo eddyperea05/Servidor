15078235b1e0c0190f8d37720bf0fd17ba40cad2
eddyperea05 <eddyperea05@gmail.com>
2021-06-10 19:31:27 -0500

    Se termina el modulo express

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const path = require('path')
 const hbs = require('hbs')
+const bodyParse = require('body-parser')
 require('./helpers')
 
 const directorioPublico = path.join(__dirname, '../public')
@@ -9,6 +10,8 @@ const directorioInclude = path.join(__dirname, '../include')
 
 app.use(express.static(directorioPublico))
 hbs.registerPartials(directorioInclude)
+
+app.use(bodyParse.urlencoded({ extended: false }))
 
 app.set('view engine', 'hbs')
 
@@ -18,13 +21,19 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/calculos', (req, res) => {
+app.post('/calculos', (req, res) => {
     console.log(req.query)
     res.render('calculos', {
-        estudiante: req.query.nombre,
-        nota1: parseInt(req.query.nota1),
-        nota2: parseInt(req.query.nota2),
-        nota3: parseInt(req.query.nota3)
+        estudiante: req.body.nombre,
+        nota1: parseInt(req.body.nota1),
+        nota2: parseInt(req.body.nota2),
+        nota3: parseInt(req.body.nota3)
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('error', {
+        estudiante: 'error'
     })
 })
 
